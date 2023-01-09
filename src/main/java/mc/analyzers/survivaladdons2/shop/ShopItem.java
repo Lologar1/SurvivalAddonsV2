e07dd918e74832e1fa30025bfbf16868a8397653
Lologar1 <38157845+Lologar1@users.noreply.github.com>
2023-01-09 17:43:19 -0500

    Add files via upload

---
+++ src/main/java/mc/analyzers/survivaladdons2/shop/ShopItem.java
@@ -0,0 +1,159 @@
+package mc.analyzers.survivaladdons2.shop;
+
+import jdk.tools.jlink.plugin.Plugin;
+import mc.analyzers.survivaladdons2.SurvivalAddons2;
+import mc.analyzers.survivaladdons2.commands.Shop;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import static mc.analyzers.survivaladdons2.shop.ShopItems.*;
+
+public class ShopItem {
+    private final ItemStack actualItem;
+    private final int priceChangeRequiredVolume;
+    private final Material type;
+    private final boolean canSell;
+    private final int basePrice;
+    private int offset;
+
+    public static ShopItem getByMaterial(Material type){
+        switch (type){
+            case NETHERITE_SCRAP:
+                return netherite_scrap;
+            case EMERALD:
+                return emerald;
+            case GOLD_INGOT:
+                return gold_ingot;
+            case COBBLESTONE:
+                return cobblestone;
+            case DIRT:
+                return dirt;
+            case LAPIS_LAZULI:
+                return lapis_lazuli;
+            case DIAMOND:
+                return diamond;
+            case IRON_INGOT:
+                return iron_ingot;
+            case BEDROCK:
+                return bedrock;
+            case COAL:
+                return coal;
+            case STRING:
+                return string;
+            case ROTTEN_FLESH:
+                return flesh;
+            case BONE:
+                return bone;
+            case BLAZE_ROD:
+                return blazerod;
+            case SPIDER_EYE:
+                return spidereye;
+            case GUNPOWDER:
+                return gunpowder;
+            case GHAST_TEAR:
+                return ghast_tear;
+            case MAGMA_CREAM:
+                return magma_cream;
+            case ENDER_PEARL:
+                return ender_pearl;
+            case QUARTZ:
+                return quartz;
+            case SLIME_BALL:
+                return slime;
+        }
+        return null;
+    }
+
+    public ShopItem(ItemStack item, int priceChangeRequiredVolume, int basePrice, boolean canSell){
+        this.canSell = canSell;
+        this.actualItem = item;
+        this.priceChangeRequiredVolume = priceChangeRequiredVolume;
+        this.type = item.getType();
+        this.basePrice = basePrice;
+        this.offset = 0;
+        SurvivalAddons2 plugin = SurvivalAddons2.getPlugin();
+        if(plugin.getConfig().get("shop." + item.getType() + ".bought") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".bought", 0);
+        }
+        if(plugin.getConfig().get("shop." + item.getType() + ".sold") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".sold", 0);
+        }
+        if(plugin.getConfig().get("shop." + item.getType() + ".transactions") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".transactions", 0);
+        }
+        if(plugin.getConfig().get("shop." + item.getType() + ".bias") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".bias", 0);
+        }
+        if(plugin.getConfig().get("shop." + item.getType() + ".offset") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".offset", 0);
+        }
+        if(plugin.getConfig().get("shop." + item.getType() + ".price") == null){
+            plugin.getConfig().set("shop." + item.getType() + ".price", basePrice);
+        }
+        plugin.getConfig().set("shop." + item.getType() + ".baseprice", basePrice);
+        SurvivalAddons2.getPlugin().saveConfig();
+    }
+
+    public boolean isCanSell(){
+        return canSell;
+    }
+
+    public double getBiasedPrice(){
+        SurvivalAddons2 plugin = SurvivalAddons2.getPlugin();
+        return Math.max(1, Math.round(plugin.getConfig().getInt("shop." + type + ".price")));
+    }
+
+    public int buyItem(int amount){
+        int dustPrice = 0;
+        SurvivalAddons2 plugin = SurvivalAddons2.getPlugin();
+        int basePrice = plugin.getConfig().getInt("shop." + type + ".baseprice");
+        int offset = plugin.getConfig().getInt("shop." + type + ".offset");
+        int oldBias = plugin.getConfig().getInt("shop." + type + ".bias");
+        plugin.getConfig().set("shop." + type + ".bought", plugin.getConfig().getInt("shop." + type + ".bought") + amount);
+        plugin.getConfig().set("shop." + type + ".transactions", plugin.getConfig().getInt("shop." + type + ".transactions") + 1);
+        double newPrice = plugin.getConfig().getDouble("shop." + type + ".price");
+        for(int i = 0; i<amount; i++){
+            offset += 1;
+            if(offset >= priceChangeRequiredVolume){
+                oldBias+=1;
+                offset = 0;
+                newPrice = basePrice + oldBias; //+ newPrice*0.1*oldBias;
+            }
+            dustPrice += Math.round(newPrice);
+        }
+        plugin.getConfig().set("shop." + type + ".price", newPrice);
+        plugin.getConfig().set("shop." + type + ".bias", oldBias);
+        plugin.getConfig().set("shop." + type + ".offset", offset);
+        plugin.saveConfig();
+        return Math.max(1, dustPrice);
+    }
+
+    public int sellItem(int amount){
+        int dustPrice = 0;
+        SurvivalAddons2 plugin = SurvivalAddons2.getPlugin();
+        int basePrice = plugin.getConfig().getInt("shop." + type + ".baseprice");
+        int offset = plugin.getConfig().getInt("shop." + type + ".offset");
+        int oldBias = plugin.getConfig().getInt("shop." + type + ".bias");
+        plugin.getConfig().set("shop." + type + ".sold", plugin.getConfig().getInt("shop." + type + ".sold") + amount);
+        plugin.getConfig().set("shop." + type + ".transactions", plugin.getConfig().getInt("shop." + type + ".transactions") + 1);
+        double newPrice = plugin.getConfig().getDouble("shop." + type + ".price");
+        for(int i = 0; i<amount; i++){
+            dustPrice += Math.round(newPrice);
+            offset -= 1;
+            if(offset < 0){
+                oldBias-=1;
+                offset = priceChangeRequiredVolume-1;
+                newPrice = (basePrice + oldBias);//newPrice*0.1*oldBias);
+            }
+        }
+        plugin.getConfig().set("shop." + type + ".price", newPrice);
+        plugin.getConfig().set("shop." + type + ".bias", oldBias);
+        plugin.getConfig().set("shop." + type + ".offset", offset);
+        plugin.saveConfig();
+        return Math.max(0, dustPrice);
+    }
+
+    public ItemStack getActualItem() {
+        return actualItem;
+    }
+}
---
+++ src/main/java/mc/analyzers/survivaladdons2/shop/ShopItems.java
@@ -0,0 +1,31 @@
+package mc.analyzers.survivaladdons2.shop;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+import sun.jvm.hotspot.types.JDoubleField;
+
+public class ShopItems {
+    public static final ShopItem iron_ingot = new ShopItem(new ItemStack(Material.IRON_INGOT), 8, 10, true);
+    public static final ShopItem lapis_lazuli = new ShopItem(new ItemStack(Material.LAPIS_LAZULI), 16, 4, true);
+    public static final ShopItem gold_ingot = new ShopItem(new ItemStack(Material.GOLD_INGOT), 8, 6, true);
+    public static final ShopItem diamond = new ShopItem(new ItemStack(Material.DIAMOND), 8, 35, true);
+    public static final ShopItem netherite_scrap = new ShopItem(new ItemStack(Material.NETHERITE_SCRAP), 4, 175, true);
+    public static final ShopItem bedrock = new ShopItem(new ItemStack(Material.BEDROCK), 1000, 100000, true);
+    public static final ShopItem coal = new ShopItem(new ItemStack(Material.COAL), 16, 2, true);
+    public static final ShopItem bone = new ShopItem(new ItemStack(Material.BONE), 32, 2, true);
+    public static final ShopItem string = new ShopItem(new ItemStack(Material.STRING), 32, 2, true);
+    public static final ShopItem flesh = new ShopItem(new ItemStack(Material.ROTTEN_FLESH), 32, 2, true);
+    public static final ShopItem emerald = new ShopItem(new ItemStack(Material.EMERALD), 16, 5, true);
+    public static final ShopItem gunpowder = new ShopItem(new ItemStack(Material.GUNPOWDER), 32, 5, true);
+    public static final ShopItem blazerod = new ShopItem(new ItemStack(Material.BLAZE_ROD), 8, 2, true);
+    public static final ShopItem spidereye = new ShopItem(new ItemStack(Material.SPIDER_EYE), 32, 2, true);
+    public static final ShopItem magma_cream = new ShopItem(new ItemStack(Material.MAGMA_CREAM), 16, 4, true);
+    public static final ShopItem ghast_tear = new ShopItem(new ItemStack(Material.GHAST_TEAR), 8, 5, true);
+    public static final ShopItem quartz = new ShopItem(new ItemStack(Material.QUARTZ),16, 3, true);
+    public static final ShopItem ender_pearl = new ShopItem(new ItemStack(Material.ENDER_PEARL), 32, 5, true);
+    public static final ShopItem slime = new ShopItem(new ItemStack(Material.SLIME_BALL), 16, 6, true);
+
+    public static final ShopItem dirt = new ShopItem(new ItemStack(Material.DIRT), 100000000, 1, false);
+    public static final ShopItem cobblestone = new ShopItem(new ItemStack(Material.COBBLESTONE), 100000000, 2, false);
+}
